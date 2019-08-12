a3643e449fc6ce90756c54c0a86e310b7176a8b8
yuchuan zhuang <yuchuan.zhuang@mindgeek.com>
2019-08-12 14:50:48 -0400

    create working text editor by working aroud some issues

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import MyEditor from "./MyEditor";
+import "./App.css";
 
 function App() {
   return (
-    <div>
+    <div className="editorContainer">
       <MyEditor />
     </div>
   );
